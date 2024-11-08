747d8aabcbf3c870c35874a7f5441c76bb05ba47
Geilson Lucas <geilson.gears@outlook.com>
2024-11-08 08:19:08 -0300

    Update trabalhopratico2.js

--- trabalhopratico2.js
+++ trabalhopratico2.js
@@ -5,7 +5,7 @@ function gerarAleatorios(){
   while(vetor.length < 6){
     var aleatorio = Math.floor(Math.random()*60 + 1);
     geracoes.push(aleatorio);
-    if(vector.includes(aleatorio)){
+    if(vetor.includes(aleatorio)){
       continue;
     }else{
       vetor.push(aleatorio);
